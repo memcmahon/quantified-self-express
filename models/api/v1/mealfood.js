40c8bf0443c3a284f3df8008fbc973ba3ccf0c62
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-05-07 14:57:44 -0600

    adds destroy function to mealfood model

--- models/api/v1/mealfood.js
+++ models/api/v1/mealfood.js
@@ -5,8 +5,13 @@ const database = require('knex')(configuration);
 const create = (meal_id, food_id) => {
   return database.raw(`INSERT INTO mealfoods (meal_id, food_id, created_at, updated_at) VALUES(?, ?, ?, ?)`,
                       [meal_id, food_id, new Date, new Date])
+};
+
+const destroy = (meal_id, food_id) => {
+  return database.raw(`DELETE FROM mealfoods WHERE meal_id = ${meal_id} AND food_id = ${food_id}`)
 }
 
 module.exports = {
-  create
+  create,
+  destroy
 }
