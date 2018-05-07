e2529c7b5ad503dfcb225306745fdf83c8386398
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-05-07 13:48:13 -0600

    Adds delete controller route

--- controllers/api/v1/foods.js
+++ controllers/api/v1/foods.js
@@ -3,37 +3,45 @@ const configuration = require('../../../knexfile')[environment];
 const database = require('knex')(configuration);
 const Food = require('../../../models/api/v1/food');
 
-const getFoods = (req, res) => {
+const index = (req, res) => {
   Food.all()
     .then((data) => {
       res.status(200).json(data.rows)
     })
 };
 
-const getFood = (req, res) => {
-  Food.find(req.params["id"])
+const show = (req, res) => {
+  Food.find(req.params.id)
     .then((data) => {
       res.status(200).json(data.rows[0])
     })
 };
 
-const createFood = (req, res) => {
+const create = (req, res) => {
   Food.create(req.body.name, req.body.calories)
     .then((data) => {
       res.status(200).json(data.rows[0])
     })
 };
 
-const updateFood = (req, res) => {
-  Food.update(req.params["id"], req.body.name, req.body.calories)
+const update = (req, res) => {
+  Food.update(req.params.id, req.body.name, req.body.calories)
     .then((data) => {
       res.status(200).json(data.rows[0])
     })
 };
 
+const destroy = (req, res) => {
+  Food.destroy(req.params.id)
+    .then((data) => {
+      res.sendStatus(204)
+    })
+};
+
 module.exports = {
-  getFoods,
-  getFood,
-  createFood,
-  updateFood
+  index,
+  show,
+  create,
+  update,
+  destroy
 }
